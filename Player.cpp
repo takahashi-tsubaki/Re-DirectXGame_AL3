7646b07fa915614d8bdc0e53b3dd2460e714f778
LC1B_15_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2022-06-01 16:28:55 +0900

    移動制限を追加

--- Player.cpp
+++ Player.cpp
@@ -55,6 +55,16 @@ void Player::Update()
 	
 	worldTransform_.matWorld_.operator*=(matTrans);
 	
+	//ˆÚ“®§ŒÀ
+	const float kMoveLimitX = 36.0f;
+	const float kMoveLimitY = 19.0f;
+
+	worldTransform_.translation_.x = max(worldTransform_.translation_.x, -kMoveLimitX);
+	worldTransform_.translation_.x = min(worldTransform_.translation_.x, +kMoveLimitX);
+
+	worldTransform_.translation_.y = max(worldTransform_.translation_.y, -kMoveLimitY);
+	worldTransform_.translation_.y = min(worldTransform_.translation_.y, +kMoveLimitY);
+
 	//s—ñ‚ÌXV
 	worldTransform_.TransferMatrix();
 
