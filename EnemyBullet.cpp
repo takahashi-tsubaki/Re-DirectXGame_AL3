86894f939c40812a8050cff4d6884f2e0f7ab422
LC1B_15_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2022-06-27 22:34:56 +0900

    一定間隔ごとに球を発射できた

---
+++ EnemyBullet.cpp
@@ -0,0 +1,39 @@
+#include "EnemyBullet.h"
+void EnemyBullet::Init(Model* model, const Vector3& position, const Vector3& velocity) 
+{
+	// NULLポインタ
+	assert(model);
+
+	//
+	model_ = model;
+	//テクスチャハンドルの読み込み
+	textureHandle_ = TextureManager::Load("EnemyBullet.png");
+
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	//引数で受けとった初期座標をセット
+	worldTransform_.translation_ = {position.x, position.y, position.z};
+	/*affinMat.translate = affin::generateTransMat(worldTransform_);*/
+
+	//引数で受け取った速度をメンバ変数に代入
+	velocity_ = velocity;
+}
+
+void EnemyBullet::Update() 
+{
+	worldTransform_.matWorld_ = MathUtility::Matrix4Identity();
+
+	//座標を移動させる
+	worldTransform_.translation_ -= velocity_;
+	affinMat.translate = affin::generateTransMat(worldTransform_);
+	affin::setTransformationWolrdMat(affinMat, worldTransform_);
+	//行列の合成
+	worldTransform_.TransferMatrix();
+
+
+}
+
+void EnemyBullet::Draw(const ViewProjection& viewProjection) 
+{
+	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+}
