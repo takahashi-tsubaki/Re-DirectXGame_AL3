ab4fa44ce81f7c51002ee8f5f4b5dffae4312b88
LC1B_15_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2022-08-20 03:25:22 +0900

    要らない部分の削除

--- skydome.cpp
+++ skydome.cpp
@@ -6,10 +6,20 @@ void skydome::Init(Model* model) {
 
 	worldTransform_.Initialize();
 
+	//“V‹…‚ÌŠg‘å
+	worldTransform_.scale_ = {2, 2, 2};
+	Matrix4 matScale;
+
+	matScale = affin::generateScaleMat(worldTransform_);
+
+	worldTransform_.matWorld_.operator*=(matScale);
+	//s—ñ‚Ì“]‘—
+	worldTransform_.TransferMatrix();
 }
 
 void skydome::Update()
-{
+{ 
+
 	
 }
 
